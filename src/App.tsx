36b5b40cbf38d8a8b87815c4d2f0eabb7c9a3492
Nick Papadakis <orfneos@yahoo.gr>
2025-06-09 21:57:14 +0300

    feat: using states in components

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,32 @@
 // import FunctionalComponent from "./components/FunctionalComponent.tsx";
 // import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
 // import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
-import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+// import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+
+// import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
+import Layout from "./components/Layout.tsx";
+// import FunctionalComponent from "./components/FunctionalComponent.tsx";
+import FunctionalComponent from "./components/FunctionalComponentWithState.tsx";
+
 
 function App() {
 
   return (
     <>
+        <Layout>
     {/*<ClassComponent/>*/}
     {/*  <FunctionalComponent/>*/}
     {/*  <ArrowFunctionalComponent/>*/}
     {/*    <ArrowFunctionalComponentWithProps title="Is a Arrow Functional Component With Props!"/>*/}
-        <ArrowFunctionalComponentWithPropsType
-            title="Is a Arrow Functional Component With Props!"
-            description="this is a description"/>
+    {/*    <ArrowFunctionalComponentWithPropsType*/}
+    {/*        title="Is a Arrow Functional Component With Props!"*/}
+    {/*        description="this is a description"/>*/}
+    {/*    <ClassComponentWithState/>*/}
+
+            <FunctionalComponent/>
+
+
+        </Layout>
     </>
   )
 }
